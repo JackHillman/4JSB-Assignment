45f46172f75c0c48010ff316a64c72202d91d40d
JackHillman <jack.hillman@me.com>
2015-10-14 22:10:09 +1030

    added form functions

--- js/filldates.js
+++ js/filldates.js
@@ -1,13 +1,25 @@
-var date = new Date();
-var year = date.getFullYear();
-var month = date.getMonth();
+var monthName = new Array()
+
+monthName[0] = "January";
+monthName[1] = "February";
+monthName[2] = "March";
+monthName[3] = "April";
+monthName[4] = "May";
+monthName[5] = "June";
+monthName[6] = "July";
+monthName[7] = "August";
+monthName[8] = "September";
+monthName[9] = "October";
+monthName[10] = "November";
+monthName[11] = "December";
+
 
 //Year
 
 function makeYears(location)
 {
 	location = document.getElementById(location);
-	for (i = year-5; i <= year+5; i++)
+	for (var i = year-5; i <= year+5; i++)
 	{
 		var opt = document.createElement('option');
 		opt.value = i;
@@ -25,12 +37,12 @@ function makeYears(location)
 function makeMonths(location)
 {	
 	location = document.getElementById(location);
-	for (i = 1; i <= 12; i++)
+	for (var i = 0; i <= 11; i++)
 	{
 		var opt = document.createElement('option');
 		opt.value = i;
-		opt.innerHTML = i;
-		if (i === month+1)
+		opt.innerHTML = monthName[i]
+		if (i === month)
 		{
 			opt.selected = true;
 		}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,85 @@
+var date = new Date(),
+	year = date.getFullYear(),
+	month = date.getMonth()
+
 function submitCheck() {
-	console.log("test");
-	return true;
+	var info = {
+		fname: document.forms["form"]["fname"].value,
+		lname: document.forms["form"]["lname"].value,
+		pcode: document.forms["form"]["pcode"].value,
+		email: document.forms["form"]["email"].value,
+		cctype: document.forms["form"]["cctype"].value,
+		ccnum: document.forms["form"]["ccnum"].value,
+		ccv: document.forms["form"]["ccv"].value,
+		month: document.forms["form"]["month"].value,
+		year: document.forms["form"]["year"].value
+	}
+	
+	var old = document.getElementById("err");
+	if (old)
+	{
+		old.parentNode.removeChild(old);
+	}
+	
+	// Check if any are empty
+	for (key in info)
+	{
+		// If value is empty
+		if (!info[key])
+		{
+			// Disable submit
+			return false;
+		}
+	}
+	
+	var loc = document.getElementById("buttons");
+	var final = document.getElementById("main");
+	var regex = /\S+@\S+\.\S+/;
+	
+	function makeError(main, body)
+	{
+		var err = document.createElement("div");
+		err.className = "alert alert-danger";
+		err.id = "err"
+		err.innerHTML = "<strong>" + main + " </strong>" + body;
+		loc.appendChild(err);
+	}
+	
+	if (!regex.test(info.email))
+	{
+		makeError("Error!", "Invalid email address!");
+		document.forms["form"]["email"].select();
+		return false
+	}
+	else if (info.pcode.length != 4)
+	{
+		makeError("Error!", "Postcode must be a 4 digit number!");
+		document.forms["form"]["pcode"].select();
+		return false;
+	}
+	else if (info.ccnum.length != 16)
+	{
+		makeError("Error!", "Credit card number must be valid!");
+		document.forms["form"]["ccnum"].select();
+		return false;
+	}
+	else if (info.ccv.length != 3)
+	{
+		makeError("Error!", "CCV must be 3 digits!");
+		document.forms["form"]["ccv"].select();
+		return false;
+	}
+	else if (info.year < year || (info.year = year && info.month < month))
+	{
+		makeError("Error!", "Credit card has expired!");
+		return false;
+	}
+	else{
+		document.getElementById("form").className = "hide";
+		var success = document.createElement("div");
+		success.className = "alert alert-success";
+		success.innerHTML = "<strong>Success!</strong> You've successfully submitted this form!";
+		final.appendChild(success);
+	}
+	return false;
 }
